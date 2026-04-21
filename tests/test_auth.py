72e4dc31bedd812c3b1b3fdf9ddc7c54cd7a21b7
Glebbse <pearsebrosnan7@gmail.com>
2026-04-21 14:45:41 +0300

    add test login with invalid credentials

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -27,3 +27,37 @@ async def test_register_duplicate_username_returns_409(client):
     data = second_response.json()
 
     assert "detail" in data
+
+@pytest.mark.asyncio
+async def test_login_returns_access_token(client):
+    payload = {"username": "gleb", "password": "gleb123"}
+
+    reg_response = await client.post("/auth/register", json=payload)
+    assert reg_response.status_code == 200
+
+    login_response = await client.post("/auth/login", json=payload)
+    assert login_response.status_code == 200
+
+    data = login_response.json()
+    assert "access_token" in data
+    assert data["token_type"] == "bearer"
+
+@pytest.mark.asyncio
+async def test_login_with_invalid_credentials_returns_401(client):
+    reg_payload = {"username": "gleb", "password": "gleb123"}
+    wrong_pw_payload = {"username": "gleb", "password": "gleb122"}
+    wrong_username_payload = {"username": "glwb", "password": "gleb123"}
+
+    reg_response = await client.post("/auth/register", json=reg_payload)
+    assert reg_response.status_code == 200
+
+    wrong_pw_response = await client.post("/auth/login", json=wrong_pw_payload)
+    assert wrong_pw_response.status_code == 401
+    data1 = wrong_pw_response.json()
+    assert data1["detail"] == "Invalid username or password"
+
+    wrong_username_response = await client.post("/auth/login", json=wrong_username_payload)
+    assert wrong_username_response.status_code == 401
+    data2 = wrong_username_response.json()
+    assert data2["detail"] == "Invalid username or password"
+
